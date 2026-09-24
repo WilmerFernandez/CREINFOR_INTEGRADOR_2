29e539ac998218d5dba1d54b040a23a3f27e9982
PLANET ONLINE <PLANET ONLINE@DESKTOP-EBC6CF4.mshome.net>
2026-09-23 21:14:32 -0500

    Comit Inicial

---
+++ src/main/java/arq/ori/serv/creinfor_integrador_2/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package arq.ori.serv.creinfor_integrador_2.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
